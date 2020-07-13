213a7ca27d1b791bed02aa8d4ebae080c3aabbf2
clicube <clicube@gmail.com>
2020-07-13 16:08:36 +0900

    Improve timeline design

--- lib/presentation/main/parts/timeline_status.dart
+++ lib/presentation/main/parts/timeline_status.dart
@@ -10,30 +10,82 @@ class StatusWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(4),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(
-            width: 36,
-            height: 36,
-            child: Image.network(status.account.avatar.toString()),
+          Column(
+            children: [
+              const SizedBox(height: 4),
+              Container(
+                width: 36,
+                height: 36,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 0.5,
+                  ),
+                  borderRadius: BorderRadius.circular(100),
+                  image: DecorationImage(
+                    image: Image.network(
+                      status.account.avatarStatic.toString(),
+                    ).image,
+                  ),
+                ),
+              ),
+            ],
           ),
-          const SizedBox(width: 4),
+          const SizedBox(width: 8),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  status.account.displayName,
-                  textAlign: TextAlign.start,
-                ),
+                _StatusHeader(status: status),
+                const SizedBox(height: 4),
                 _StatusContent(status: status),
+                const Divider(height: 16, color: Colors.grey),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _StatusHeader extends StatelessWidget {
+  const _StatusHeader({@required this.status, Key key}) : super(key: key);
+  final Status status;
+
+  @override
+  Widget build(BuildContext context) {
+    final baseTextStyle =
+        Theme.of(context).textTheme.bodyText1.copyWith(color: Colors.grey);
+    return Row(
+      children: [
+        RichText(
+          text: MastodonTextSpan(
+            text: status.account.displayName,
+            emojis: status.account.emojis,
+            emojiSize: Theme.of(context).textTheme.bodyText1.fontSize * 1.2,
+            style: baseTextStyle.copyWith(fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(width: 8),
+        Text(
+          '@${status.account.username}',
+          style: baseTextStyle,
+        ),
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              '24s',
+              style: baseTextStyle,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -48,8 +100,8 @@ class _StatusContent extends StatelessWidget {
       text: MastodonTextSpan(
         text: status.content,
         emojis: status.emojis,
-        emojiSize: 16,
-        style: DefaultTextStyle.of(context).style,
+        emojiSize: Theme.of(context).textTheme.bodyText1.fontSize * 1.2,
+        style: Theme.of(context).textTheme.bodyText1,
         onTapLink: (url) {
           FlutterWebBrowser.openWebPage(url: url);
         },
